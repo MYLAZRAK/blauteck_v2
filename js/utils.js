564ef996e4ac4d5b75a88b5711a8148fca14d75c
MYLAZRAK <148683149+MYLAZRAK@users.noreply.github.com>
2026-01-16 14:57:05 +0100

    Create utils.js

---
+++ js/utils.js
@@ -0,0 +1,15 @@
+// Scroll to top function
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+// Get URL parameters
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
